349e72f6c244645fc15fd40c41f890320abf68cb
cdvalencias <127420819+cdvalencias@users.noreply.github.com>
2023-06-03 15:35:31 -0500

    funciones
    consultar saldo , historial y consignar

--- scriptSaldo.js
+++ scriptSaldo.js
@@ -1,13 +1,11 @@
 // funcion para consultar saldo
 const saldoUsuario = 10000000
+let valorConsignar = document.querySelector(".valorConsignar")
 let consultarSaldo = document.querySelector("#consultarSaldo")
 consultarSaldo.addEventListener("click",saldo)
 
-function saldo(e) {
-    e.preventDefault();
-    let saldos = document.querySelector(".saldo")
-    saldos.value = "su saldo es " + saldoUsuario
-
+//funcion que crea tabla del historial
+function tablas() {
     var today = new Date();
 
     let tr = document.createElement("tr")
@@ -23,4 +21,65 @@ function saldo(e) {
     tr.classList.add(".tablaOperaciones")
     tr.append(tdFecha,tdCajero,tdOperaciones)
     tabla.append(tr)
+}
+
+//funcion que oculta la taba del historial
+function OcultarTablaHistoria() {
+    tabla = document.querySelector(".tablaOperaciones")
+    tabla.style.display="none"
+}
+// funcion que muestra el saldo y oculta la tabla del historial
+function saldo(e) {
+    e.preventDefault();
+    let saldos = document.querySelector(".saldo")
+    saldos.value = "su saldo es " + saldoUsuario
+
+    tablas()
+    OcultarTablaHistoria()
  }
+//creacion de evento que muestra el historial
+let consultarOperaciones = document.querySelector("#consultarOperaciones")
+consultarOperaciones.addEventListener("click",operaciones)
+
+function operaciones(e) {
+    e.preventDefault();
+    let initabla = document.querySelector("#iniTabla")
+    initabla.style.display="block" 
+    tabla = document.querySelector(".tablaOperaciones")
+    tabla.style.display="block"
+}
+
+// creacion de evento y funcion que muentran condignaciones
+let menuConsignar = document.querySelector(".consignar")
+menuConsignar.addEventListener("click",consignar)
+
+function alertaConsignacion()
+    {
+    var mensaje;
+    var opcion = confirm("estas seguro que deseas continuar con la cosignacion");
+    if (opcion == true) {
+        mensaje = "se ha consignado con exito";
+        tablas()
+	} else {
+	    mensaje = "se cancelo la consigancion";
+	}
+	document.getElementById("mensajeConsigancion").innerHTML = mensaje;
+}
+
+function consignar(e) {
+    e.preventDefault();
+    let menuConsignar = document.querySelector(".menuConsignar")
+    menuConsignar.style.display="block" 
+    OcultarTablaHistoria()
+    valorConsignar = document.querySelector(".valorConsignar")
+    valorConsignar.value 
+    console.log(valorConsignar.value)  
+}
+let aceptarconsignacion = document.querySelector(".aceptarConsignacion")
+aceptarconsignacion.addEventListener("click",sola)
+function sola() {
+    console.log(valorConsignar.value)
+    if (valorConsignar.value>0) {
+        alertaConsignacion()
+    }  
+}
