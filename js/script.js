614d9c65beeefbb8b768b75a625df9f60dec656a
cdvalencias <127420819+cdvalencias@users.noreply.github.com>
2023-06-07 19:31:48 -0500

    arreglo problemas de login

--- js/script.js
+++ js/script.js
@@ -1,47 +1,34 @@
-// let currentUser = 1
-
-// switch (currentUser) {
-//     case 1:
-//         location.href="login.html"
-//         break;
-// }
 let usuarios=["jhon","christian","jhonatan"]
 let passwords=[1111,2222,3333]
 
 let usuario=document.querySelector("#usuario")
 let password=document.querySelector("#password")
 
-// let nombreUsuario = document.querySelector("#nombre_Usuario")
-
-
-
-
 let aceptar=document.querySelector("#login")
 aceptar.addEventListener("click",validar)
 
 let intentos = 0
-
+let i = 0
 function validar(e) {
     e.preventDefault();
     let errorLogin=document.querySelector("#error")  
-    if (intentos < 2) {   
+    if (intentos < 3) {   
         intentos = intentos + 1
-        for (let i =0 ; i <= usuarios.length ; i++) { 
+        for (i =0 ; i <= 2 ; i++) { 
             if (usuarios[i]== usuario.value && passwords[i]==password.value) {
                 currentUser=usuario.value
                 location.href="menu.html"
-                // nombreUsuario = document.querySelector("#nombre_Usuario")
-                // nombreUsuario.textContent="usuario[i]"
 
             }else{  
                                               
                 errorLogin.textContent="usuario o contraseña no valido"
-                password.value = ""
-            }            
-        }              
+                
+            } 
+            //password.value = ""           
+        }
+        password.value = ""                
     }else{
         errorLogin.textContent="has superado el limite de intentos"
-        password.value = ""
         aceptar.style.display = "none";
     }
 }
